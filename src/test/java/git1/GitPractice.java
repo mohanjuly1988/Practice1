d69e7baf7c4c60260624d794fcb7be8fd283d974
Pratap-999 <muvvapratapkumar@gmail.com>
2025-04-27 16:21:10 +0530

    updated code

--- src/test/java/git1/GitPractice.java
+++ src/test/java/git1/GitPractice.java
@@ -5,6 +5,7 @@ public class GitPractice {
 	public static void main(String[] args) 
 	{
 		System.out.println("This code is added by your Sr.SDET");
+		System.out.println("Git practice 27th april");
 	
 	}
 
